117a28ed1b00994ded8ff6323dfa6bc3ab83892c
unknown <Deborah389JDIW@outlook.com>
2026-05-02 01:10:28 -0700

    End commit120 visitor data

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-   server: {
-        host: '0.0.0.0', // يبقى هكذا ليسمح بالدخول من الخارج
-        port: 5173,
-        strictPort: true,
-        hmr: {
-            // استبدل هذا بـ IP جهازك الحقيقي (IPv4)
-            host: '192.168.1.110', 
-        },
-    },
+//    server: {
+//         host: '0.0.0.0', // يبقى هكذا ليسمح بالدخول من الخارج
+//         port: 5173,
+//         strictPort: true,
+//         hmr: {
+//             // استبدل هذا بـ IP جهازك الحقيقي (IPv4)
+//             host: '192.168.1.110', 
+//         },
+//     },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx', // تأكد أن امتداد ملفك هو .jsx أو .js حسب مشروعك
